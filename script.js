4c33b7b0094bc01f923a0ae50763764fa96dbdc8
Nina Li <ninal1811@hstat.org>
2021-06-12 21:29:00 +0000

    update repo

--- script.js
+++ script.js
@@ -16,55 +16,51 @@ function toDo(text){
   output.appendChild(trashIcon);
   trashIcon.classList.add('gg-trash');
   
-  // edit icon
-  var editIcon = document.createElement('span');
-  output.appendChild(editIcon);
-  editIcon.classList.add('gg-pen');
-  
   // when trash icon is clicked, deletes task
   var allTrash = document.querySelectorAll('.gg-trash');
-  
   allTrash.forEach(function(allTrash, trashIcon){
     allTrash.addEventListener("click",function(event){
-      console.log("delete");
       var remove = event.target.parentElement;
       remove.style.display = "none";
+      
+      db.collection("todolist").get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+          console.log(remove);
+        });
+      });
     });
   });
   
-  // when edit icon is clicked, edit task
-  var allEdit = document.querySelectorAll('.gg-pen');
-  
-  allEdit.forEach(function(allEdit, editIcon){
-    allEdit.addEventListener("click",function(){
-      console.log("edit");
-      document.querySelector('#todo').appendChild(output).contentEditable = true;
-    });
-  });
-    
   // when check icon is clicked, moves task to complete
   var allComplete = document.querySelectorAll('.gg-check-r');
-  
   allComplete.forEach(function(allComplete, checkIcon){
-    allComplete.addEventListener("click",function(){
-      console.log("complete");
-      document.querySelector('#complete').appendChild(output);
+    allComplete.addEventListener("click",function(event){
+      document.querySelector('#complete').appendChild(event.target.parentElement);
+      
+      db.collection('todolist').get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+          console.log(complete);
+        });
+      });
     });
   });
   output.innerHTML += text;
-
+  
   // resets input box when add is clicked
   document.getElementById('task').value = "";
+  document.getElementById('due-date').value = "";
 }
 
 document.getElementById('add').addEventListener("click", function(){
   var text = "";
   text += document.querySelector('#task').value;
-  toDo(text);
+  var date = "   ";
+  date += document.querySelector('#due-date').value;
+  toDo(text + date);
   
   // setting into Firebase
   db.collection('todolist').add({
-    input: text
+    input: text + date
   });
 });
 
@@ -74,5 +70,42 @@ db.collection('todolist').get().then((snapshot) => {
     console.log(doc.data());
     var text = doc.data().input;
     toDo(text);
+    
+    // allows the last task to be deleted
+    var allTrash = document.querySelectorAll('.gg-trash');
+    allTrash.forEach(function(allTrash, trashIcon){
+      allTrash.addEventListener("click",function(event){
+        var remove = event.target.parentElement;
+        remove.style.display = "none";
+        
+        // deletes tasks in Firebase
+        db.collection("todolist").get().then((snapshot) => {
+          snapshot.docs.forEach(doc => {
+            if (remove.textContent == doc.data().input){
+              console.log(doc.data().input + " is removed");
+              db.collection("todolist").doc(doc.id).delete();
+            }
+          });
+        });
+      });
+    });
+    
+    // when check icon is clicked, moves task to complete
+    var allComplete = document.querySelectorAll('.gg-check-r');
+    allComplete.forEach(function(allComplete, checkIcon){
+      allComplete.addEventListener("click",function(event){
+        document.querySelector('#complete').appendChild(event.target.parentElement);
+        
+        // moves to complete, deletes task in Firebase
+        db.collection('todolist').get().then((snapshot) => {
+          snapshot.docs.forEach(doc => {
+            if (complete.textContent == doc.data().input){
+              console.log(doc.data().input + " is completed");
+              db.collection("todolist").doc(doc.id).delete();
+            }
+          });
+        });
+      });
+    });
   });
 });
